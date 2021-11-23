3d9fe1d015d56894de6b6e11d72fc1b7f1e137da
Ji <csy960407@naver.com>
2021-11-24 01:24:48 +0900

    NoteView 기능 구현중

--- app/src/main/java/com/example/studybnb/NoteListActivity.kt
+++ app/src/main/java/com/example/studybnb/NoteListActivity.kt
@@ -3,12 +3,19 @@ package com.example.studybnb
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
+    private var firestore : FirebaseFirestore? = null
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
 
         back_btn.setOnClickListener {
             finish()
@@ -18,6 +25,10 @@ class NoteListActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.setReverseLayout(true)
+        layoutManager.setStackFromEnd(true)
+        diary_list.layoutManager = layoutManager
+        diary_list.adapter = ItemAdapter(this)
     }
 }
--- app/src/main/java/com/example/studybnb/NoteViewActivity.kt
+++ app/src/main/java/com/example/studybnb/NoteViewActivity.kt
@@ -1,11 +1,78 @@
 package com.example.studybnb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_note_view.*
+import java.text.SimpleDateFormat
 
 class NoteViewActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    var firestore : FirebaseFirestore = FirebaseFirestore.getInstance()
+    var storage : FirebaseStorage?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_view)
+        auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+        var date : String? = null
+
+        back_btn.setOnClickListener {
+            finish()
+        }
+
+        firestore?.collection("Records")?.whereEqualTo("visit_date",date?.toLong())//1619271434933
+            ?.get()?.addOnSuccessListener { documents ->
+                for(doc in documents){
+                    title_txt.text = doc?.data?.get("note_title").toString()
+                    date_txt.text = SimpleDateFormat("yyyy.MM.dd").format(doc?.data?.get("visit_date"))
+                    contents_txt.text = doc?.data?.get("note_title").toString()
+
+                    var storageRef = storage?.reference?.child("images")?.child(doc?.data?.get("img_src").toString())
+                    storageRef?.downloadUrl?.addOnSuccessListener { uri ->
+                        Glide.with(applicationContext)
+                            .load(uri)
+                            .into(img_view)
+                        Log.v("IMAGE","Success")
+                    }?.addOnFailureListener { //이미지 로드 실패
+                        Toast.makeText(applicationContext, "실패", Toast.LENGTH_SHORT).show()
+                        Log.v("IMAGE","failed")
+
+                    }
+                    //break
+                }
+            }
+
+        delete_btn.setOnClickListener {
+            val mAlertDialog = AlertDialog.Builder(this)
+            mAlertDialog.setTitle("삭제")
+            mAlertDialog.setMessage("삭제된 노트 기록은 복구할 수 없으며 데이터베이스에서 완전히 삭제됩니다. " +
+                    "정말 삭제하겠습니까?")
+            mAlertDialog.setPositiveButton("Yes") { dialog, id ->
+                //perform some tasks here
+                firestore.collection("Records")?.document("record_${auth.currentUser?.uid}_${date}")
+                    .delete()
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "삭제되었습니다.",Toast.LENGTH_LONG).show()
+                    }
+                val intent = Intent(this, NoteListActivity :: class.java )
+                startActivity(intent)
+                finish()
+            }
+
+            mAlertDialog.setNegativeButton("No") { dialog, id ->
+                //perform som tasks here
+            }
+            mAlertDialog.show()
+        }
+
     }
 }
