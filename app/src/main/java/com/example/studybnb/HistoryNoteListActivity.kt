9e0f8ff8be54e33eeba4953de4396b2906b2c1f8
Ji <csy960407@naver.com>
2021-11-29 23:17:39 +0900

    한국사 노트테이킹 기능 추가

---
+++ app/src/main/java/com/example/studybnb/HistoryNoteListActivity.kt
@@ -0,0 +1,40 @@
+package com.example.studybnb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.studybnb.adapter.HistoryItemAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class HistoryNoteListActivity : AppCompatActivity() {
+    private var firestore : FirebaseFirestore? = null
+    private lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_note_list)
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        back_btn.setOnClickListener {
+            myStartActivity(SubjectSelectActivity::class.java)
+        }
+        write_btn.setOnClickListener {
+            myStartActivity(HistoryNoteWriteActivity::class.java)
+        }
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.setReverseLayout(true)
+        layoutManager.setStackFromEnd(true)
+        diary_list.layoutManager = layoutManager
+        diary_list.adapter = HistoryItemAdapter(this)
+    }
+
+    private fun myStartActivity(c: Class<*>) {
+        val intent = Intent(this, c)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) //액티비티가 여러게 쌓이는 것을 방지. 뒤로가기 누르면 앱 종료.
+        startActivity(intent)
+    }
+
+}
