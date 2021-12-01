19822477452bd1047ab0072877ac22afd1f6f994
Michael Sklors <skmi1013@hs-karlsruhe.de>
2021-12-01 17:15:38 +0900

    Firestore storing like Hankyu

--- app/src/main/java/com/example/studybnb/model/StudyTimerModel.kt
+++ app/src/main/java/com/example/studybnb/model/StudyTimerModel.kt
@@ -3,9 +3,8 @@ package com.example.studybnb.model
 import java.util.Date
 
 data class StudyTimerModel(
-    var UID : String? = null,
-    var study_time : Long? = null,
-    var break_time : Long? = null,
+    var start_time : Long? = null,
+    var finish_time : Long? = null,
     var date: String? = null,
     var subject: String? = null
 )
